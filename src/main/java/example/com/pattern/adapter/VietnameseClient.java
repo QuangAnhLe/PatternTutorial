826f8c96eedd42d98a82c2f2c88d200df91c05dc
Quang Anh Le <quanganh.le24@gmail.com>
2021-05-16 14:50:59 +0200

    aufräumen

--- src/main/java/example/com/pattern/adapter/VietnameseClient.java
+++
@@ -1,9 +0,0 @@
-package example.com.pattern.adapter;
-
-public class VietnameseClient {
-    public static void main (String[] args){
-        VietnameseTarget client = new TranslatorAdapter(new JapaneseAdaptee());
-        client.send("xin chao");
-
-    }
-}
--- src/main/java/example/com/pattern/observer/ObserverPatternDemo.java
+++
@@ -1,7 +0,0 @@
-package example.com.pattern.observer;
-
-public class ObserverPatternDemo {
-    public static void main (String[] args){
-
-    }
-}
