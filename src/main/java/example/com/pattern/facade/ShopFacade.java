aa60f6e1d94847761dcdead345270db5a4a16d11
Quang Anh Le <quanganh.le24@gmail.com>
2021-05-22 01:53:10 +0200

    Facade initial

---
+++ src/main/java/example/com/pattern/facade/ShopFacade.java
@@ -0,0 +1,42 @@
+package example.com.pattern.facade;
+
+public class ShopFacade {
+    private static final ShopFacade INSTANCE = new ShopFacade();
+
+    private AccountService accountService;
+    private PaymentService paymentService;
+    private ShippingService shippingService;
+    private EmailService emailService;
+    private SmsService smsService;
+
+    private ShopFacade() {
+        accountService = new AccountService();
+        paymentService = new PaymentService();
+        shippingService = new ShippingService();
+        emailService = new EmailService();
+        smsService = new SmsService();
+    }
+
+    public static ShopFacade getInstance() {
+        return INSTANCE;
+    }
+
+    public String buyProductByCashWithFreeShipping(String email) {
+        accountService.getAccount(email);
+        paymentService.paymentByCash();
+        shippingService.freeShipping();
+        emailService.sendMail(email);
+        System.out.println("Done\n");
+        return email;
+    }
+
+    public String buyProductByPaypalWithStandardShipping(String email, String mobilePhone) {
+        accountService.getAccount(email);
+        paymentService.paymentByPaypal();
+        shippingService.standardShipping();
+        emailService.sendMail(email);
+        smsService.sendSMS(mobilePhone);
+        System.out.println("Done\n");
+        return mobilePhone;
+    }
+}
