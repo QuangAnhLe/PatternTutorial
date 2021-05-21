364932028c97abd760a1b0e8bfde73f68a279504
Quang Anh Le <quanganh.le24@gmail.com>
2021-05-22 01:52:21 +0200

    Sub-System initial

---
+++ src/main/java/example/com/pattern/facade/ShippingService.java
@@ -0,0 +1,16 @@
+package example.com.pattern.facade;
+
+public class ShippingService {
+
+    public void freeShipping() {
+        System.out.println("Free Shipping");
+    }
+
+    public void standardShipping() {
+        System.out.println("Standard Shipping");
+    }
+
+    public void expressShipping() {
+        System.out.println("Express Shipping");
+    }
+}
